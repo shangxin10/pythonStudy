7c8ad95af0be953e6a39fbd77208abc5c1c9d12a
vector <295253797@qq.com>
2017-07-16 23:34:06 +0800

    first day

--- day01/helloworld.py
+++ day01/helloworld.py
@@ -1,2 +1,48 @@
-print(1+7);
-print("helloword");
+
+#print(1+7);
+#print("helloword");
+
+
+#name = input();
+#print(name);
+
+# a = 100;
+# if a >= 0:
+#    print(a);
+# else:
+#    print(-a);
+
+
+# print("I'm vector,\n nice to meet you");
+
+# print(True and False)
+# print(True or False)
+# print(not True)
+
+# a = None;
+# print(a);
+
+# n = 123;
+# f = 456.789;
+# s1 = 'Hello world';
+# s2 = 'Hello \'Adam\'';
+# s3 = r'Hello, "Bart"';
+# s4 = r'''Hello,
+# Lisa!'''
+#
+# print(n);
+# print(f);
+# print(s1);
+# print(s2);
+# print(s3);
+# print(s4);
+
+# print(ord("A"));
+# print(ord("中"));
+#
+# print(chr(66));
+# print(chr(25991));
+
+
+x = b'ABC';
+print(x);
